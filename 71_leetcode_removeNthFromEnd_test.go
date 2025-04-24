a737369f121104b2cf7df2dc9a83bef236348514
WeiXinao <1632967698@qq.com>
2025-04-24 16:51:28 +0800

    feat: removeNthFromEnd

---
+++ 71_leetcode_removeNthFromEnd_test.go
@@ -0,0 +1,26 @@
+package test
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	if head.Next == nil {
+		return nil
+	}
+	var (
+		p = head
+		q = head
+	)
+	for ;n > 0; n-- {
+		q = q.Next		
+	}
+	if q == nil && n > 0 {
+		return nil
+	}
+	if q == nil && n == 0 {
+		return head.Next
+	}
+	for q.Next != nil {
+		p = p.Next
+		q = q.Next
+	}
+	p.Next = p.Next.Next
+	return head
+}
